ef0dca070f787785d8c5de203ef5e2bd708e4666
innuz <innuz@vestas.com>
2023-01-09 08:31:01 +0100

    Prototipe - add handlers to buttons

--- MainPage.xaml.cs
+++ MainPage.xaml.cs
@@ -1,8 +1,27 @@
-﻿namespace GameTimer;
+﻿using System.Collections.Generic;
+
+namespace GameTimer;
 
 public partial class MainPage : ContentPage
 {
 	int count = 0;
+	int players_count = 4;
+	bool pause = false;
+	turn_add_time = 0;
+	Dictionary<int, string> players = new Dictionary<int, string>
+		{
+			{ 1, "White" },
+			{ 2, "Black" },
+			{ 3, "Yellow" },
+			{ 4, "Blue" },
+			{ 5, "Red" },
+			{ 6, "Green" },
+			{ 7, "Purple" },
+			{ 8, "Orange" },
+		};
+
+
+
 
 	public MainPage()
 	{
@@ -23,12 +42,17 @@ public partial class MainPage : ContentPage
 
 	private void OpenSettings(object sender, EventArgs e)
 	{
-		count++;
+		if (players_count != 8)
+			players_count++;
+		else players_count = 1;
+		SettingsBtn.Text = $"Players: {players_count}";
+
 	}
 
 	private void PauseGame(object sender, EventArgs e)
 	{
-		count++;
+		PauseBtn.Text = pause ? "Resume" : "Pause";
+		pause = !pause;
 	}
 }
 
